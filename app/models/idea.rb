779f598ec779c97ba62a3d484513ac6842155264
mthuctem <mthuctem@gmail.com>
2017-09-17 08:57:32 +0000

    remove max length

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ApplicationRecord
     has_many :comments
     mount_uploader :picture, PictureUploader
-    validates :name, presence: true, length: {maximum: 6, minimum: 1}
+    validates :name, presence: true
 end
